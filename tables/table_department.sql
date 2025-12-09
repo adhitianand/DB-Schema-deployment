be1547823658c0eddafdfec876fa93c51d290ea3
adhitianand <106898431+adhitianand@users.noreply.github.com>
2025-12-09 20:35:50 +0530

    Create table_department.sql

---
+++ tables/table_department.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS department (
+    department_id SERIAL PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
